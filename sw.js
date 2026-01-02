5096589260bedef29196637f809776ba94b3204d
Marco <madcarbone@gmail.com>
2026-01-02 10:26:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,3 @@
-const CACHE = "rider-v1";
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll([
-      "./",
-      "./index.html",
-      "./manifest.json",
-      "./icon.svg"
-    ]))
-  );
-});
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
